36884bde0d0fdb53eb675fbf057b85111ac01081
Jayetosin <Jayetosin>
2023-02-19 17:03:07 +0100

    index.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Javascript-Operator
+Arithmetic Operator (+ , - , * , / )
+Assignment Operator( = , -= , += , /= ,*= , %= , ^= ,|=)
+Comparism Operator ( <, > , >== ,<== ,!==)
+Logical Operator ( && And , || Or , ! Not)
+Bitwise operator ( & Bitwise And ,!  Bitwise inclusive OR , ^ Bitwise exclusive OR ,<< Left Shift, >> Right Shift  , ~ Bitwise NOT)
---
+++ index.js
@@ -0,0 +1,71 @@
+//ARITHEMETIC OPERATOR\\
+
+let x = 5;
+let y = 7;
+let =  x + y ;
+console.log( x + y );
+
+let a = 6;
+let b = 5;
+let = a - b;
+console.log ( a - b );
+
+//Assignment Operator
+
+let m = 6;
+let n = 2;
+let = m *= n;
+console.log( m *= n);
+
+
+let p = 10;
+let q = 5;
+let = p /= q;
+console.log( p /= q );
+
+
+//Comparism Operator
+let z = 24;
+console.log(y > 50);
+
+let d = 100;
+console.log(d !== 50);
+
+//Logical Operator
+
+
+// Bitwise Operator
+let f= 10;
+console.log(f  >> 90);
+
+let k= 70;
+console.log(k & 35);
+
+
+
+//Question 3 If...else
+ 
+let Subjects= 'Arts Subjects';
+
+let ScienceSubjects = 'Physics, Chemistry, Biology, Technical Drawing';
+let SocialScienceSubjects = 'Accounting, Commerce, Marketing, Geography';
+let ArtsSubjects = 'Government, Economics, Literature, History';
+let GeneralSubjects = 'English, Mathematics';
+
+if (Subjects) {
+    console.log(GeneralSubjects + ' ' + ArtsSubjects)  ;  
+} else {
+    console.log('GeneralSubjects');
+}
+
+
+//Question 4
+for ( let i = 1; i < 20; i += 7){
+    console.log(i);
+}
+//the result is 1 , 8 , 15
+
+//Question 5
+if  (num = 20 , pwr = 10);
+console.log( 'the number 10 is the power of 2 nearest to 20')
+
